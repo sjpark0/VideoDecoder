7fc3f8d72cbd62308d187f44ee76100e182fb086
sjpark <sjpark0@gmail.com>
2023-05-13 21:39:42 +0900

    수정

--- VideoDecoder/FrameDecoding.cpp
+++ VideoDecoder/FrameDecoding.cpp
@@ -176,12 +176,13 @@ int NaiveDecoding(int argc, char* argv[]) {
     while (av_read_frame(format_ctx, &packet) >= 0) {
         if (packet.stream_index == video_stream_index) {
             int response = avcodec_send_packet(codec_ctx, &packet);
-            if (response < 0) {
+			if (response < 0) {
                 throw std::runtime_error("Error sending packet for decoding");
             }
 
             while (response >= 0) {
                 response = avcodec_receive_frame(codec_ctx, frame);
+				//printf("%d\n", frame->best_effort_timestamp);
                 if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
                     break;
                 }
@@ -239,96 +240,133 @@ int FastDecoding(int argc, char* argv[]) {
 
     //av_register_all();
 
-    AVFormatContext* format_ctx = nullptr;
-    if (avformat_open_input(&format_ctx, input_filename, nullptr, nullptr) != 0) {
-        throw std::runtime_error("Cannot open input file");
-    }
+	AVFormatContext *format_ctx = avformat_alloc_context();
+	if (avformat_open_input(&format_ctx, input_filename, nullptr, nullptr) != 0) {
+		std::cerr << "Failed to open input file." << std::endl;
+		return -1;
+	}
 
-    if (avformat_find_stream_info(format_ctx, nullptr) < 0) {
-        throw std::runtime_error("Cannot find stream information");
-    }
+	// 스트림 정보 찾기
+	if (avformat_find_stream_info(format_ctx, nullptr) < 0) {
+		std::cerr << "Failed to find stream information." << std::endl;
+		return -1;
+	}
 
-    int video_stream_index = -1;
-    AVCodecParameters* codec_params = nullptr;
-    for (unsigned int i = 0; i < format_ctx->nb_streams; i++) {
-        if (format_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
-            video_stream_index = i;
-            codec_params = format_ctx->streams[i]->codecpar;
-            break;
-        }
-    }
+	// 비디오 스트림 찾기
+	int video_stream_index = -1;
+	AVCodecParameters *codec_params = nullptr;
+	for (unsigned int i = 0; i < format_ctx->nb_streams; ++i) {
+		if (format_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+			video_stream_index = i;
+			codec_params = format_ctx->streams[i]->codecpar;
+			break;
+		}
+	}
 
-    if (video_stream_index == -1) {
-        throw std::runtime_error("Cannot find video stream");
-    }
+	if (video_stream_index == -1) {
+		std::cerr << "Failed to find a video stream." << std::endl;
+		return -1;
+	}
+
+	AVStream *video_stream = format_ctx->streams[video_stream_index];
+
 
     const AVCodec* codec = avcodec_find_decoder(codec_params->codec_id);
     if (!codec) {
         throw std::runtime_error("Cannot find codec");
+		return -1;
     }
+	    
+	// 코덱 컨텍스트 설정
+	AVCodecContext *codec_ctx = avcodec_alloc_context3(codec);
+	if (avcodec_parameters_to_context(codec_ctx, codec_params) < 0) {
+		std::cerr << "Failed to set codec context." << std::endl;
+		return -1;
+	}
 
-    AVCodecContext* codec_ctx = avcodec_alloc_context3(codec);
-    if (!codec_ctx) {
-        throw std::runtime_error("Cannot allocate codec context");
-    }
 
-    if (avcodec_parameters_to_context(codec_ctx, codec_params) < 0) {
-        throw std::runtime_error("Cannot copy codec parameters to codec context");
-    }
-
-    if (avcodec_open2(codec_ctx, codec, nullptr) < 0) {
-        throw std::runtime_error("Cannot open codec");
-    }
-    AVStream* video_stream = format_ctx->streams[video_stream_index];
+	// 디코더 열기
+	if (avcodec_open2(codec_ctx, codec, nullptr) < 0) {
+		std::cerr << "Failed to open decoder." << std::endl;
+		return -1;
+	}
     
     int user_input_frame_number = atoi(argv[2]);
+	int64_t target_pts = user_input_frame_number * video_stream->time_base.den / video_stream->time_base.num / 30;
+	
+	// i-프레임을 찾습니다.
+	int64_t iframe_pts = av_index_search_timestamp(video_stream, target_pts, AVSEEK_FLAG_BACKWARD);
+	
+	printf("%d\n", iframe_pts);
 
-    int64_t target_pts = user_input_frame_number * video_stream->time_base.den / (video_stream->time_base.num * 30);
-    
-    // i-프레임을 찾습니다.
-    int64_t iframe_pts = av_index_search_timestamp(video_stream, target_pts, AVSEEK_FLAG_BACKWARD);
-    printf("T : %d, %d\n", video_stream->time_base.den, video_stream->time_base.num);
-    // 탐색을 위한 위치를 설정합니다.
-    AVRational av_time_base_q = { 1, AV_TIME_BASE };
-    int64_t seek_target = av_rescale_q(iframe_pts, video_stream->time_base, av_time_base_q);
-    av_seek_frame(format_ctx, video_stream_index, seek_target, AVSEEK_FLAG_BACKWARD);
+	// 가장 가까운 I-프레임으로 이동
+	if (av_seek_frame(format_ctx, video_stream_index, iframe_pts * video_stream->time_base.den / video_stream->time_base.num / 30, AVSEEK_FLAG_BACKWARD) < 0) {
+		std::cerr << "Failed to seek to the nearest I-frame." << std::endl;
+		return -1;
+	}
 
-    printf("%d\n", seek_target);
-
-    AVFrame* frame = av_frame_alloc();
-    if (!frame) {
-        throw std::runtime_error("Cannot allocate frame");
-    }
-
-    int current_frame_number = 0;
     AVPacket packet;
-    int decoded_frame_number = 0;
+    AVFrame *frame = av_frame_alloc();
+	
+	// 이미지 저장을 위한 SwsContext 설정
+	SwsContext *sws_ctx = sws_getContext(codec_ctx->width, codec_ctx->height, codec_ctx->pix_fmt,	codec_ctx->width, codec_ctx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, nullptr, nullptr, nullptr);
 
-    while (av_read_frame(format_ctx, &packet) >= 0) {
-        if (packet.stream_index == video_stream_index) {
-            avcodec_send_packet(codec_ctx, &packet);
+	// RGB 버퍼 준비
+	uint8_t *rgb_data = (uint8_t *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 1));
+	AVFrame *rgb_frame = av_frame_alloc();
+	av_image_fill_arrays(rgb_frame->data, rgb_frame->linesize, rgb_data, AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 1);
 
-            while (avcodec_receive_frame(codec_ctx, frame) == 0) {
-                printf("%d, %d\n", frame->best_effort_timestamp, target_pts);
-                // 원하는 프레임을 찾으면 저장하고 루프를 종료합니다.
-                if (frame->best_effort_timestamp >= target_pts) {
-                    save_frame(frame, codec_ctx->width, codec_ctx->height, user_input_frame_number);
-                    break;
-                }
-            }
-        }
-        av_packet_unref(&packet);
+	int decoded_frame_count = 0;
+	bool frame_found = false;
+	while (!frame_found && av_read_frame(format_ctx, &packet) >= 0) {
+		if (packet.stream_index == video_stream_index) {
+			if (avcodec_send_packet(codec_ctx, &packet) == 0) {
+				while (avcodec_receive_frame(codec_ctx, frame) == 0) {
+					printf("%d, %d\n", frame->best_effort_timestamp, target_pts);
+					if (frame->pts >= target_pts) {
+						frame_found = true;
+						break;
+					}
+				}
+			}
+		}
+		av_packet_unref(&packet);
 
-        // 원하는 프레임을 이미 찾았으면 루프를 종료합니다.
-        if (frame->best_effort_timestamp >= target_pts) {
-            break;
-        }
-    }
+		if (frame_found) {
+			AVFrame* frame_rgb = av_frame_alloc();
+			if (!frame_rgb) {
+				throw std::runtime_error("Cannot allocate RGB frame");
+			}
 
-    av_frame_free(&frame);
-    avcodec_close(codec_ctx);
-    avcodec_free_context(&codec_ctx);
-    avformat_close_input(&format_ctx);
+			uint8_t* buffer = nullptr;
+			int num_bytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 1);
+			buffer = (uint8_t*)av_malloc(num_bytes * sizeof(uint8_t));
 
-    return 0;
+			av_image_fill_arrays(frame_rgb->data, frame_rgb->linesize, buffer, AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 1);
+
+			SwsContext* sws_ctx = sws_getContext(codec_ctx->width, codec_ctx->height, codec_ctx->pix_fmt, codec_ctx->width, codec_ctx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, nullptr, nullptr, nullptr);
+			sws_scale(sws_ctx, frame->data, frame->linesize, 0, codec_ctx->height, frame_rgb->data, frame_rgb->linesize);
+
+			save_frame(frame_rgb, codec_ctx->width, codec_ctx->height, 1);
+			std::cout << "Saved frame " << 1 << " as an image file." << std::endl;
+
+			av_frame_free(&frame_rgb);
+			av_free(buffer);
+			sws_freeContext(sws_ctx);
+			break;
+		}
+	}
+	
+
+	// 메모리 해제 및 종료
+	av_frame_free(&frame);
+	av_frame_free(&rgb_frame);
+	av_free(rgb_data);
+	sws_freeContext(sws_ctx);
+	avcodec_close(codec_ctx);
+	avcodec_free_context(&codec_ctx);
+	avformat_close_input(&format_ctx);
+	avformat_free_context(format_ctx);
+
+	return 0;
 }
